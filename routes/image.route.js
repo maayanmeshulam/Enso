55f9f07b7a4697655fb944e596e09164b4f69e09
maayanmeshulam <maayanmeshulam@gmail.com>
2021-07-25 15:47:18 +0300

    resource image - get combination

--- routes/image.route.js
+++ routes/image.route.js
@@ -86,5 +86,67 @@ imageExpressRoute.route('/remove-image/:imageName').delete((req, res, next) => {
 })
 
 // Get Combination
+imageExpressRoute.route('/image-combination').get((req, res) => {
+    ImageSchema.find({}, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+
+            var names = []
+            data.forEach(function(doc) {
+                    names.push(doc.name);
+                  })
+            combimationList = []
+            printCombination(names, names.length, req.query.length, combimationList);
+            res.json(combimationList)
+        }
+    })
+})
+
+
+
+
+function combinationUtil(arr,data,start,end,index,r, combimationList) {
+    // Current combination is ready to be printed, print it
+    if (index == r)
+    {
+        
+        tempArr = []
+        for (let j=0; j<r; j++)
+        {
+            tempArr[j] = data[j];
+        }
+        combimationList.push([...tempArr])
+    }
+        
+        // replace index with all possible elements. The condition
+        // "end-i+1 >= r-index" makes sure that including one element
+        // at index will make a combination with remaining elements
+        // at remaining positions
+    for (let i=start; i<=end && end-i+1 >= r-index; i++)
+    {
+        data[index] = arr[i];
+        combinationUtil(arr, data, i+1, end, index+1, r, combimationList);
+    }
+}
+      
+// The main function that prints all combinations of size r
+// in arr[] of size n. This function mainly uses combinationUtil()
+function printCombination(arr,n,r, combimationList)
+{
+    // A temporary array to store all combination one by one
+    let data = new Array(r);
+    
+    // Print all combination using temporary array 'data[]'
+    combinationUtil(arr, data, 0, n-1, 0, r, combimationList);
+}
+      
+/*Driver function to check for above function*/
+// let arr=[1, 2, 3, 4, 5];
+// let r = 3;
+// let n = arr.length;
+// combimationList = []
+// printCombination(arr, n, r, combimationList);
+// console.log(combimationList)
 
 module.exports = imageExpressRoute;
